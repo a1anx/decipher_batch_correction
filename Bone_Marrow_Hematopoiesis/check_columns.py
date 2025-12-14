6e67530ef25666a3e8e587b9ef6ac7ab4e0213fd
a1anx <ajc.xie@gmail.com>
2025-12-14 11:21:19 -0500

    Organizing files part 1.4

--- Bone_Marrow_Hematopoiesis/check_columns.py
+++
@@ -1,6 +0,0 @@
-import scanpy as sc
-
-adata = sc.read_h5ad("BoneMarrowMap_small_20k_5kgenes.h5ad")
-print("Available columns in adata.obs:")
-print(adata.obs.columns.tolist())
-print(f"\nShape: {adata.shape}")
